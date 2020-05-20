92feccb69173fb17b041d32e3236c7a2c4068980
Can Umay <30483006+canumay@users.noreply.github.com>
2020-05-20 23:45:02 +0300

    Create Task.js

---
+++ models/Task.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const timestamps = require('mongoose-timestamp');
+const Joi = require('joi');
+
+var TaskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    expireAt: { type: Date, required: false },
+    label: { type: String, required: false },
+    labelType: { type: String, enum: ['danger', 'warning', 'primary', 'info', 'dark', 'success'], required: false }
+});
+
+// Auto Timestamp for createdAt, updatedAt
+TaskSchema.plugin(timestamps);
+
+TaskSchema.methods.joiValidate = (obj) => {
+    const taskSchema = Joi.object({
+        title: Joi.string().min(3).max(60).required(),
+        description: Joi.string().min(3).max(280),
+        column_id: Joi.string(),
+        expireAt: Joi.date(),
+        label: Joi.string().min(3).max(15),
+        labelType: Joi.string().valid("danger", "warning", "primary", "success", "info", "dark")
+    })
+    return Joi.validate(obj, taskSchema);
+}
+
+var Task = mongoose.model('Task', TaskSchema);
+
+exports.Task = Task;
