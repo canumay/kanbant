b7b0a341a05214e6824e4b142f4aeb7da88ed5f9
Can Umay <30483006+canumay@users.noreply.github.com>
2020-05-20 23:45:08 +0300

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,61 @@
 const Router = require('express').Router;
-const { User } = require('../models/User');
-const bcrypt = require('bcryptjs');
 const router = new Router();
+const { isLoggedIn } = require('../middlewares');
+const { User } = require('../models/User');
+const { Project } = require('../models/Project');
+const { Column } = require('../models/Column');
+const { Task } = require('../models/Task');
+const mongoose = require('mongoose');
 
-router.post('/register', async (req, res, next) => {
+router.post('/createProject', isLoggedIn, async (req, res, next) => {
     try {
-        let { email, password } = req.body;
-        let user = new User({ email, password });
-        let isExists = await User.findOne({ email });
-        if (isExists !== null) {
-            res.status(409).json({ status: false, message: "User already exists" });
-        } else {
-            await user.joiValidate({ email, password }); // Validator
-            var salt = await bcrypt.genSaltSync(10);
-            var hashedPassword = await bcrypt.hashSync(password, salt);
-            user = new User({ email: email, password: hashedPassword });
-            var createdUser = await user.save();
-            res.status(200).json({ status: true, message: 'User created successfully' });
-        }
+        let project = new Project(req.body);
+        await project.joiValidate(req.body);
+        project = await project.save(req.body);
+        await User.findByIdAndUpdate(req.user._id, { "$push": { "projects": project._id } }, { "new": true, "upsert": true });
+        res.status(200).send({ status: true, message: 'Project successfully created.' });
 
     } catch (err) {
-        res.status(400).json({ status: false, message: err });
+        res.status(500).json({ status: false, message: err });
     }
-})
+});
+
+router.post('/createColumn', isLoggedIn, async (req, res, next) => {
+    try {
+        if (req.body.project_id && mongoose.Types.ObjectId.isValid(req.body.project_id)) {
+            let column = new Column(req.body);
+            column = await column.save(req.body);
+            await column.joiValidate(req.body);
+            await Project.findByIdAndUpdate(req.body.project_id, { "$push": { "columns": column._id } }, { "new": true, "upsert": true });
+            res.status(200).send({ status: true, message: 'Column successfully created.' });
+        } else {
+            res.status(400).json({ status: false, message: "project_id is undefined or malformed." });
+        }
+    } catch (err) {
+        res.status(500).json({ status: false, message: err });
+    }
+});
+
+
+router.post('/createTask', isLoggedIn, async (req, res, next) => {
+    try {
+        if (req.body.column_id && mongoose.Types.ObjectId.isValid(req.body.column_id)) {
+            let task = new Task(req.body);
+            await task.joiValidate(req.body);
+            task = await task.save(req.body);
+            await Column.findByIdAndUpdate(req.body.column_id, { "$push": { "tasks": task._id } }, { "new": true, "upsert": true });
+            res.status(200).send({ status: true, message: 'Task successfully created.' });
+
+        } else {
+            res.status(400).json({ status: false, message: "column_id is undefined or malformed." });
+
+        }
+
+
+    } catch (err) {
+        res.status(500).json({ status: false, message: err });
+    }
+});
+
 
 module.exports = router;
