7bbc8dbd2201eb643e2f28deb0c51f4567ecc7ab
Can Umay <30483006+canumay@users.noreply.github.com>
2020-05-20 23:45:05 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const Router = require('express').Router;
 const router = new Router();
+const { User } = require('../models/User');
+const bcrypt = require('bcryptjs');
 const passport = require('passport');
-const {isLoggedIn} = require('../middlewares');
 
 // Local Login
 router.post('/local', (req, res, next) => {
@@ -10,4 +11,25 @@ router.post('/local', (req, res, next) => {
         // failureRedirect: '/auth',
     })(req, res, next);
 });
+
+router.post('/register', async (req, res, next) => {
+    try {
+        let { email, password } = req.body;
+        let user = new User({ email, password });
+        let isExists = await User.findOne({ email });
+        if (isExists !== null) {
+            res.status(409).json({ status: false, message: "User already exists" });
+        } else {
+            await user.joiValidate({ email, password }); // Validator
+            var salt = await bcrypt.genSaltSync(10);
+            var hashedPassword = await bcrypt.hashSync(password, salt);
+            user = new User({ email: email, password: hashedPassword });
+            var createdUser = await user.save();
+            res.status(200).json({ status: true, message: 'User created successfully' });
+        }
+
+    } catch (err) {
+        res.status(400).json({ status: false, message: err });
+    }
+})
 module.exports = router;
