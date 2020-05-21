bce1b1dd874d3a9e14071682a1e6f6c23b4e1099
Can Umay <30483006+canumay@users.noreply.github.com>
2020-05-21 23:52:01 +0300

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -5,10 +5,21 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import routes from './routes';
+import axios from 'axios';
+import moment from 'vue-moment';
+
+const axiosInstance = axios.create({
+  withCredentials: true,
+  baseURL: "http://127.0.0.1:3000/api"
+})
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
+
+Vue.use(moment);
+
+Vue.prototype.$http = axiosInstance;
 
 const router = new VueRouter({
   mode: 'history',
