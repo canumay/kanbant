e8ba48fa9a8f6ad313ccec5d48f2b07b0d7bd74f
Can Umay <30483006+canumay@users.noreply.github.com>
2020-05-23 20:32:12 +0300

    ENV files (no secrets, just dev/prod seperator for vue.js)

--- client/src/main.js
+++ client/src/main.js
@@ -12,7 +12,7 @@ import VueSimpleContextMenu from 'vue-simple-context-menu'
 
 const axiosInstance = axios.create({
   withCredentials: true,
-  baseURL: "http://127.0.0.1:3000/api"
+  baseURL: process.env.VUE_APP_BASE_URL
 })
 
 Vue.config.productionTip = false
