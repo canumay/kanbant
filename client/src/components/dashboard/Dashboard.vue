0e1f77487d1bbfafb7120a11c8ae6b9d5a26fbe4
Can Umay <30483006+canumay@users.noreply.github.com>
2020-05-23 21:56:45 +0300

    Update Dashboard.vue

--- client/src/components/dashboard/Dashboard.vue
+++ client/src/components/dashboard/Dashboard.vue
@@ -720,7 +720,7 @@ export default {
   transition: all .3s ease;
 }
 .slide-fade-leave-active {
-  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all .1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 .slide-fade-enter, .slide-fade-leave-to
 /* .slide-fade-leave-active below version 2.1.8 */ {
